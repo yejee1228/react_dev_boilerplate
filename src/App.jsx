acbfb97336d08d7052b8187b5d354e3df29b8fe6
yejee1228 <yejee1228@gmail.com>
2022-12-10 23:51:46 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GlobalStyle from './GlobalStyle'
+
+const App = () => {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <div>Test !!</div>
+    </div>
+  )
+}
+
+export default App
